9a0395c4a10ab214db3e838ab5821bcc4b9751d9
ajain <ajain@BTIS1000060299>
2017-01-13 09:55:54 +0530

    Add project files.

---
+++ RPNCalc/RPNCalcTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RPNCalc
+{
+    public class RPNCalcTest
+    {
+        RPNCalculator rpnCalculatorObj = new RPNCalculator();
+
+        [Test]
+        public void TwoValueAdditionStringInput()
+        {            
+            Assert.AreEqual(8, rpnCalculatorObj.calculate("2,6,+"));
+        }
+
+        [Test]
+        public void EmptyStringInput()
+        {
+            try
+            {
+                rpnCalculatorObj.calculate("");
+            }
+            catch(Exception ex)
+            {
+                Assert.Pass(ex.Message);
+            }
+        }
+
+        [Test]
+        public void InputStringWithoutOperator()
+        {
+            try
+            {
+                rpnCalculatorObj.calculate("2,6");
+            }
+            catch (Exception ex)
+            {
+                Assert.Pass(ex.Message);
+            }
+        }
+
+        [Test]
+        public void ThreeValueAdditionStringInput()
+        {
+            Assert.AreEqual(11,rpnCalculatorObj.calculate("2,6,3,+,+"));
+        }
+
+        [Test]
+        public void ThreeValueDeductionStringInput()
+        {
+            Assert.AreEqual(-1, rpnCalculatorObj.calculate("2,6,3,-,-"));
+        }
+
+        [Test]
+        public void ThreeValueMultiplicationStringInput()
+        {
+            Assert.AreEqual(36, rpnCalculatorObj.calculate("2,6,3,*,*"));
+        }
+
+        [Test]
+        public void ThreeValueAdditionSubtractionStringInput()
+        {
+            Assert.AreEqual(-7, rpnCalculatorObj.calculate("2,6,3,+,-"));
+        }
+
+        [Test]
+        public void TwoValueAdditionInvalidStringInput()
+        {
+            try
+            {
+                rpnCalculatorObj.calculate("2,@,+");
+            }
+            catch(Exception ex)
+            {
+                Assert.Pass("Invalid operand is given",ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ RPNCalc/RPNCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace RPNCalc
+{
+    internal class RPNCalculator
+    {
+        public RPNCalculator() { }
+     
+        public int calculate(string expr)
+        {
+            int firstVal = 0, secondVal = 0;
+            Stack<int> expStack = new Stack<int>();
+ //           Regex operatorRegEx = new Regex("\d+");
+
+            string[] expSplit = expr.Split(',');
+
+            if (expr.Length == 0)
+            {
+                throw new System.Exception("String is empty");
+            }
+
+            for (int expSplitCounter = 0; expSplitCounter < expSplit.Length; expSplitCounter++)
+            {
+                if (expSplit[expSplitCounter].Equals("+") ||
+                    expSplit[expSplitCounter].Equals("-") ||
+                    expSplit[expSplitCounter].Equals("*"))
+                {
+                    secondVal = expStack.Pop();
+                    firstVal = expStack.Pop();
+
+                    switch (expSplit[expSplitCounter])
+                    {
+                        case "+":
+                            expStack.Push(firstVal + secondVal);
+                            break;
+                        case "-":
+                            expStack.Push(firstVal - secondVal);
+                            break;
+                        case "*":
+                            expStack.Push(firstVal * secondVal);
+                            break;
+                    }
+                }
+                else
+                {
+                    int inputValue = int.Parse(expSplit[expSplitCounter]);
+                    expStack.Push(inputValue);
+                }
+            }
+            return expStack.Pop();
+        }
+    }
+}
